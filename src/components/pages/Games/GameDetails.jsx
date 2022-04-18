f34762a63a4ac7085201648c50d1ca1214c0d548
Norman Teodoro <normanteodoro@hotmail.com>
2022-04-17 18:16:42 -0700

    deploy

--- src/components/pages/Games/GameDetails.jsx
+++ src/components/pages/Games/GameDetails.jsx
@@ -1,5 +1,5 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
+// import axios from 'axios'
+// import { useEffect, useState } from 'react'
 
 export default function GameDetails ({ gameId }) {
 //   const [gameDetails, setGameDetails] = useState(null)
